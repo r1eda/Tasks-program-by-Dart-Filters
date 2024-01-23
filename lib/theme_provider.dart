6cfd1b0560cc3adc2d17d45ef6d21e437c98602f
r1eda <96275386+r1eda@users.noreply.github.com>
2024-01-23 21:40:32 +0300

    Update theme_provider.dart

--- lib/theme_provider.dart
+++ lib/theme_provider.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
+// فئة ThemeProvider تمثل مزود المظهر وتمتلك وظيفة تغيير النمط والإشعار بالتغييرات.
 class ThemeProvider with ChangeNotifier {
   bool _isDarkMode = false;
 
+  // الحصول على حالة النمط الحالية (فاتح أو داكن).
   bool get isDarkMode => _isDarkMode;
 
+  // الحصول على بيانات المظهر الحالية بناءً على حالة النمط.
   ThemeData get themeData => _isDarkMode ? darkTheme : lightTheme;
 
+  // تبديل النمط بين الفاتح والداكن وإعلام المستمعين بالتغيير.
   void toggleTheme() {
     _isDarkMode = !_isDarkMode;
     notifyListeners();
   }
 
+  // تكوين المظهر الفاتح للتطبيق.
   static final ThemeData lightTheme = ThemeData(
     textTheme: TextTheme(
       bodyText1: TextStyle(
@@ -25,9 +30,10 @@ class ThemeProvider with ChangeNotifier {
         fontWeight: FontWeight.bold,
       ),
     ),
-    // Add more light theme configurations here
+    // يمكن إضافة المزيد من تكوينات المظهر الفاتح هنا.
   );
 
+  // تكوين المظهر الداكن للتطبيق.
   static final ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     textTheme: TextTheme(
@@ -42,8 +48,12 @@ class ThemeProvider with ChangeNotifier {
         fontWeight: FontWeight.bold,
       ),
     ),
-    // Add more dark theme configurations here
+    // يمكن إضافة المزيد من تكوينات المظهر الداكن هنا.
   );
-   static final IconData moonIcon = Icons.nightlight_round; // أيقونة القمر
-  static final IconData sunIcon = Icons.wb_sunny; // أيقونة الشمس
+
+  // أيقونة القمر المستخدمة عند التبديل إلى النمط الداكن.
+  static final IconData moonIcon = Icons.nightlight_round;
+
+  // أيقونة الشمس المستخدمة عند التبديل إلى النمط الفاتح.
+  static final IconData sunIcon = Icons.wb_sunny;
 }
