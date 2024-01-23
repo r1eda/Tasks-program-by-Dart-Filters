6bb084790e9c78977b8f9471ea00c9e0719603d5
r1eda <96275386+r1eda@users.noreply.github.com>
2024-01-23 21:41:38 +0300

    Update ListItem.dart

--- lib/ListItem.dart
+++ lib/ListItem.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/foundation.dart';
+
+// فئة ListItem تمثل عنصرًا فرديًا في قائمة وتحتوي على بياناته وحالة تحققه.
 class ListItem {
   int? id;
   String? title;
   String? description;
   ValueNotifier<bool> isCheckedNotifier;
 
-  ListItem({this.id, this.title, this.description, bool isChecked = false,
+  // البناء الرئيسي لإنشاء عنصر ListItem مع القيم الافتراضية.
+  ListItem({
+    this.id,
+    this.title,
+    this.description,
+    bool isChecked = false,
   }) : isCheckedNotifier = ValueNotifier<bool>(isChecked);
 
+  // تحويل بيانات العنصر إلى تنسيق JSON.
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'title': title,
       'description': description,
-       'isChecked': isCheckedNotifier.value,
+      'isChecked': isCheckedNotifier.value,
     };
   }
 
+  // تكوين عنصر ListItem باستخدام بيانات JSON.
   factory ListItem.fromJson(Map<String, dynamic> json) {
     return ListItem(
       id: json['id'],
